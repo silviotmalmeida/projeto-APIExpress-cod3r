1ca391ba00c6eb9cc870ce96b1e404f003a87d74
Silvio Almeida <silviotmalmeida@gmail.com>
2021-01-14 22:39:35 -0300

    Commit inicial

---
+++ app/src/servidor.js
@@ -0,0 +1,46 @@
+const porta = 3003
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/produtos', (req, res, next) => {
+	res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+	res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+	const produto = bancoDeDados.salvarProduto({
+		nome: req.body.nome,
+		preco: req.body.preco
+	})
+	console.log(produto)
+	res.send(produto) // JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+	const produto = bancoDeDados.salvarProduto({
+		id: req.params.id,
+		nome: req.body.nome,
+		preco: req.body.preco
+	})
+	res.send(produto) // JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+	const produto = bancoDeDados.excluirProduto(req.params.id)
+	res.send(produto) // JSON
+})
+
+app.listen(porta, () => {
+	console.log(`Servidor está executando na porta ${porta}.`)
+	console.log('A requisição deverá ser feita na url /produtos')
+	console.log('Os atributos dos produtos a serem cadastrados são: nome e preco.')
+
+})
---
+++ startContainers.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Subindo o container..."
+docker-compose up -d --remove-orphans
+
+sleep 5
+
+echo "Definindo permissoes das pastas de volumes..."
+docker container exec apiexpress bash -c "chmod 777 -R /usr/src/app"
+sleep 1
+
+echo "Iniciando o app..."
+docker container exec apiexpress bash -c "cd /usr/src/app; npm i; npm start"
+
