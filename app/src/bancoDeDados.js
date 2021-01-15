f1841a5dd020681a3d2ee8d95d100e73a3ab6ad8
Silvio Almeida <silviotmalmeida@gmail.com>
2021-01-15 13:35:56 -0300

    Adicionando comentarios ao codigo

--- app/src/bancoDeDados.js
+++ app/src/bancoDeDados.js
@@ -1,28 +1,56 @@
+//módulo que simula um banco de dados
+
+//objeto responsável por gerenciar o id autoincrementável
 const sequence = {
-	_id: 1,
-	get id() { return this._id++ }
-}
+  //notação de atributo privado
+  _id: 1,
 
-const produtos = {}
+  //implementação do método get
+  //acessível através de sequence.id
+  get id() {
+    return this._id++;
+  },
+};
 
+//inicializando o banco zerado
+const produtos = {};
+
+//função responsável pelo insert ou update do produto
 function salvarProduto(produto) {
-	if (!produto.id) produto.id = sequence.id
-	produtos[produto.id] = produto
-	return produto
+  //se o id não estiver setado, ativa o auto incremento do id
+  if (!produto.id) produto.id = sequence.id;
+
+  //inserindo ou atualizando o registro da posição referente ao id
+  produtos[produto.id] = produto;
+
+  //retornando o registro atualizado
+  return produto;
 }
 
+//função responsável pela consulta de um produto pelo id
 function getProduto(id) {
-	return produtos[id] || {}
+  //se o id estiver setado, retorna o referido registro,
+  //senão retorna um objeto vazio
+  return produtos[id] || {};
 }
 
+//função responsável por retornar a lista completa de produtos
 function getProdutos() {
-	return Object.values(produtos)
+  //retornando os valores dos produtos
+  return Object.values(produtos);
 }
 
+//função responsável por apagar um produto pelo id
 function excluirProduto(id) {
-	const produto = produtos[id]
-	delete produtos[id]
-	return produto
+  //obtendo o produto selecionado
+  const produto = produtos[id];
+
+  //apagando o registro da posição referente ao id
+  delete produtos[id];
+
+  //retornando o registro apagado
+  return produto;
 }
 
-module.exports = { salvarProduto, getProduto, getProdutos, excluirProduto }
+//exportando as funções para utilização por outros módulos
+module.exports = { salvarProduto, getProduto, getProdutos, excluirProduto };
--- app/src/servidor.js
+++ app/src/servidor.js
@@ -1,48 +1,86 @@
+//módulo principal do app
+
+//definindo a porta
 const porta = 3003;
 
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const bancoDeDados = require("./bancoDeDados");
+//definindo a url principal
+const url = "produtos";
 
-const url = 'produtos'
+//importando o express
+//responsável por criar o servidor web
+const express = require("express");
+
+//iniciando o servidor web
+const app = express();
+
+//importando o body-parser
+//responsável por tratar o body da requisição
+const bodyParser = require("body-parser");
+
+//importando o banco de dados simulado
+const bancoDeDados = require("./bancoDeDados");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get(`/${url}`, (req, res, next) => {
+//implementando o método GET (consultar)
+//a resposta será formatada em um JSON
+app.get(`/${url}`, (req, res) => {
+  //respondendo com todos os registros do banco de dados
   res.send(bancoDeDados.getProdutos());
 });
 
-app.get(`/${url}/:id`, (req, res, next) => {
+//implementando o método GET (consultar) com um filtro :id
+//a resposta será formatada em um JSON
+app.get(`/${url}/:id`, (req, res) => {
+  //respondendo com o registro referente ao id informado na request
   res.send(bancoDeDados.getProduto(req.params.id));
 });
 
+//implementando o método POST (incluir)
+//a resposta será formatada em um JSON
 app.post(`/${url}`, (req, res, next) => {
+  //criando o novo objeto com os dados dos parâmetros inserido no body da requisição
   const produto = bancoDeDados.salvarProduto({
     nome: req.body.nome,
     preco: req.body.preco,
   });
-  console.log(produto);
-  res.send(produto); // JSON
+
+  //respondendo com o registro criado
+  res.send(produto);
 });
 
+//implementando o método PUT (editar) com um filtro :id
+//a resposta será formatada em um JSON
 app.put(`/${url}/:id`, (req, res, next) => {
+  //editando o objeto com os dados dos parâmetros inserido no body da requisição
+  //a partir do id informado na request
   const produto = bancoDeDados.salvarProduto({
     id: req.params.id,
     nome: req.body.nome,
     preco: req.body.preco,
   });
-  res.send(produto); // JSON
+
+  //respondendo com o registro editado
+  res.send(produto);
 });
 
+//implementando o método DELETE (apagar) com um filtro :id
+//a resposta será formatada em um JSON
 app.delete(`/${url}/:id`, (req, res, next) => {
+  //apagando o registro referente ao id informado na request
   const produto = bancoDeDados.excluirProduto(req.params.id);
-  res.send(produto); // JSON
+
+  //respondendo com o registro apagado
+  res.send(produto);
 });
 
+//configurando o servidor para escutar a porta pré-definida
 app.listen(porta, () => {
+  //enviando mensagens pelo terminal
   console.log(`Servidor está executando na porta ${porta}.`);
-  console.log(`A requisição deverá ser feita na url http://localhost:${porta}/${url}`);
+  console.log(
+    `A requisição deverá ser feita na url http://localhost:${porta}/${url}`
+  );
   console.log(
     "Os atributos dos produtos a serem cadastrados são: nome e preco."
   );
