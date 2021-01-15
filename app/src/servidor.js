8cd1ad495d8eed7467a4a3aa8a0befd8401809ae
Silvio Almeida <silviotmalmeida@gmail.com>
2021-01-15 11:12:15 -0300

    Ajustes gerais

--- app/src/servidor.js
+++ app/src/servidor.js
@@ -1,46 +1,49 @@
-const porta = 3003
+const porta = 3003;
 
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const bancoDeDados = require('./bancoDeDados')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const bancoDeDados = require("./bancoDeDados");
 
-app.use(bodyParser.urlencoded({ extended: true }))
+const url = 'produtos'
 
-app.get('/produtos', (req, res, next) => {
-	res.send(bancoDeDados.getProdutos())
-})
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/produtos/:id', (req, res, next) => {
-	res.send(bancoDeDados.getProduto(req.params.id))
-})
+app.get(`/${url}`, (req, res, next) => {
+  res.send(bancoDeDados.getProdutos());
+});
 
-app.post('/produtos', (req, res, next) => {
-	const produto = bancoDeDados.salvarProduto({
-		nome: req.body.nome,
-		preco: req.body.preco
-	})
-	console.log(produto)
-	res.send(produto) // JSON
-})
+app.get(`/${url}/:id`, (req, res, next) => {
+  res.send(bancoDeDados.getProduto(req.params.id));
+});
 
-app.put('/produtos/:id', (req, res, next) => {
-	const produto = bancoDeDados.salvarProduto({
-		id: req.params.id,
-		nome: req.body.nome,
-		preco: req.body.preco
-	})
-	res.send(produto) // JSON
-})
+app.post(`/${url}`, (req, res, next) => {
+  const produto = bancoDeDados.salvarProduto({
+    nome: req.body.nome,
+    preco: req.body.preco,
+  });
+  console.log(produto);
+  res.send(produto); // JSON
+});
 
-app.delete('/produtos/:id', (req, res, next) => {
-	const produto = bancoDeDados.excluirProduto(req.params.id)
-	res.send(produto) // JSON
-})
+app.put(`/${url}/:id`, (req, res, next) => {
+  const produto = bancoDeDados.salvarProduto({
+    id: req.params.id,
+    nome: req.body.nome,
+    preco: req.body.preco,
+  });
+  res.send(produto); // JSON
+});
+
+app.delete(`/${url}/:id`, (req, res, next) => {
+  const produto = bancoDeDados.excluirProduto(req.params.id);
+  res.send(produto); // JSON
+});
 
 app.listen(porta, () => {
-	console.log(`Servidor está executando na porta ${porta}.`)
-	console.log('A requisição deverá ser feita na url /produtos')
-	console.log('Os atributos dos produtos a serem cadastrados são: nome e preco.')
-
-})
+  console.log(`Servidor está executando na porta ${porta}.`);
+  console.log(`A requisição deverá ser feita na url http://localhost:${porta}/${url}`);
+  console.log(
+    "Os atributos dos produtos a serem cadastrados são: nome e preco."
+  );
+});
